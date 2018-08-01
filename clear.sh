318cc7a1419f949c93da1ce2e6817c3c173df422
Kenan SALTIK <ksaltik@hotmail.com>
2018-08-02 00:46:45 +0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -14,6 +14,7 @@
     # Light Gray   0;37     White         1;37
     RED='\033[0;31m'
     NC='\033[0m' # No Color
+    ORANGE='\033[0;31m'
     lsblk
     sleep 5  # Waits 5 seconds.
     echo "You need more than maybe ${RED}50GB of space to build your own RPM packages"
@@ -30,10 +31,10 @@
         esac
     done
     echo -e "We will resize the partition now \n${RED}using parted and resizepart.${NC} \nFirst use ( p ) to print out the partitions."
-    echo -e "Then if warning message appears \nabout  - not all space available - ${RED}etc select ( Fix )${NC}"
-    echo -e "After Fix there will be info about disk \n${RED}size like - Disk /dev/sda: 67.8GB - "
+    echo -e "Then if warning message appears \nabout  - not all space available - ${ORANGE}etc select ( Fix )${NC}"
+    echo -e "After Fix there will be info about disk \n${ORANGE}size like - Disk /dev/sda: 67.8GB -${NC} "
     echo -e "Then enter resize ( partition number ) \nthe partition number most probably ${RED}2 so"
-    echo -e "${RED}Enter resizepart 2 and it will ask you the end? \nenter the above ${RED}value 67.8GB and hit enter and then q to quit"
+    echo -e "${RED}Enter resizepart 2 ${NC} and it will ask you ${RED}the end? ${NC}\nenter the above ${RED}value 67.8GB ${NC}and hit enter and then q to quit"
     sudo parted
     # The partition  resizedwas sda2
     sudo resize2fs -p /dev/sda2

93fd9e2d9ae89d97e7eb8e716bcf6ff60c360193
Kenan SALTIK <ksaltik@hotmail.com>
2018-08-01 23:41:11 +0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -8,7 +8,7 @@ lsblk
 sleep 5  # Waits 5 seconds.
 echo "You need more than maybe 50GB ıf space to build your own RPM packages and build custom bundles."
 echo "You need to install storage utils to extend partition size?"
-select yn in "Yes" "No"; do
+select yn in "Yes (1)" "No (2)"; do
     case $yn in
         Yes ) sudo swupd bundle-add storage-utils; break;;
         No ) exit;;

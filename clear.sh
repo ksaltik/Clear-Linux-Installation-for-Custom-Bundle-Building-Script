76af55858d0ce1daaf964b7df56cf73417533843
Kenan SALTIK <ksaltik@hotmail.com>
2018-08-02 00:55:54 +0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -4,14 +4,8 @@
     # Enable sshd to login from ssh client
     # echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
     # systemctl restart sshd
-    # Black        0;30     Dark Gray     1;30
-    # Red          0;31     Light Red     1;31
-    # Green        0;32     Light Green   1;32
-    # Brown/Orange 0;33     Yellow        1;33
-    # Blue         0;34     Light Blue    1;34#
-    # Purple       0;35     Light Purple  1;35
-    # Cyan         0;36     Light Cyan    1;36
-    # Light Gray   0;37     White         1;37
+    # Color codes etc
+    #https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
     RED='\033[0;31m'
     NC='\033[0m' # No Color
     ORANGE='\033[0;33m'

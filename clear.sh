faa0376f1e106cc0a3e610d87dec82b354f56ba9
Kenan SALTIK <ksaltik@hotmail.com>
2018-08-02 00:41:37 +0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,34 +1,44 @@
-#!/bin/bash
-# Get root privileges
-# sudo -s
-# Enable sshd to login from ssh client
-# echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-# systemctl restart sshd
-lsblk
-sleep 5  # Waits 5 seconds.
-echo "You need more than maybe 50GB ıf space to build your own RPM packages"
-echo "You need to install storage utils to extend partition size?"
-options=("Yes" "No")
-select yn in "${options[@]}"
-do
-    case $yn in
-        "Yes") sudo swupd bundle-add storage-utils; break;;
-#       "No" ) exit;;
-        "No" ) break;;
+    #!/bin/bash
+    # Get root privileges
+    # sudo -s
+    # Enable sshd to login from ssh client
+    # echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+    # systemctl restart sshd
+    # Black        0;30     Dark Gray     1;30
+    # Red          0;31     Light Red     1;31
+    # Green        0;32     Light Green   1;32
+    # Brown/Orange 0;33     Yellow        1;33
+    # Blue         0;34     Light Blue    1;34#
+    # Purple       0;35     Light Purple  1;35
+    # Cyan         0;36     Light Cyan    1;36
+    # Light Gray   0;37     White         1;37
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+    lsblk
+    sleep 5  # Waits 5 seconds.
+    echo "You need more than maybe ${RED}50GB of space to build your own RPM packages"
+    echo "You need to install storage utils to extend partition size?"
+    options=("Yes" "No")
+    select yn in "${options[@]}"
+    do
+        case $yn in
+            "Yes") sudo swupd bundle-add storage-utils; break;;
+    #       "No" ) exit;;
+            "No" ) break;;
 
-            *) echo "Use 1 or 2 options";continue;;
-    esac
-done
-echo -e "We will resize the partition now \nusing parted and resizepart. \nFirst use ( p ) to print out the partitions."
-echo -e "Then if warning message appears \nabout  - not all space available - etc select ( Fix )"
-echo -e "After Fix there will be info about disk \nsize like - Disk /dev/sda: 67.8GB - "
-echo -e "Then enter resize ( partition number ) \nthe partition number most probably 2 so"
-echo -e "Enter resizepart 2 and it will ask you the end? \nenter the above value 67.8GB and hit enter and then q to quit"
-sudo parted
-# The partition  resizedwas sda2
-sudo resize2fs -p /dev/sda2
-# Check the size of the disk 
-df -h
-swupd bundle-add kernel-lts
-clr-boot-manager set-timeout 10
-clr-boot-manager update
+                *) echo "Use 1 or 2 options";continue;;
+        esac
+    done
+    echo -e "We will resize the partition now \n${RED}using parted and resizepart.${NC} \nFirst use ( p ) to print out the partitions."
+    echo -e "Then if warning message appears \nabout  - not all space available - ${RED}etc select ( Fix )${NC}"
+    echo -e "After Fix there will be info about disk \n${RED}size like - Disk /dev/sda: 67.8GB - "
+    echo -e "Then enter resize ( partition number ) \nthe partition number most probably ${RED}2 so"
+    echo -e "${RED}Enter resizepart 2 and it will ask you the end? \nenter the above ${RED}value 67.8GB and hit enter and then q to quit"
+    sudo parted
+    # The partition  resizedwas sda2
+    sudo resize2fs -p /dev/sda2
+    # Check the size of the disk 
+    df -h
+    swupd bundle-add kernel-lts
+    clr-boot-manager set-timeout 10
+    clr-boot-manager update

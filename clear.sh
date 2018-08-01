b749aee0575749c1375dae3af9d070395026e227
Kenan SALTIK <ksaltik@hotmail.com>
2018-08-01 23:10:56 +0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+lsblk
+sleep 5  # Waits 5 seconds.
+echo "You need more than maybe 50GB ıf space to build your own RPM packages and build custom bundles."
+echo "You need to install storage utils to extend partition size?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) sudo swupd bundle-add storage-utils; break;;
+        No ) exit;;
+    esac
+done
 swupd bundle-add kernel-lts
 clr-boot-manager set-timeout 10
 clr-boot-manager update

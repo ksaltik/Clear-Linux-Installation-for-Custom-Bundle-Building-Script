3cc2cfbe990e4e145ccc60b617cf43942c7b437c
Kenan SALTIK <ksaltik@hotmail.com>
2018-08-02 00:14:25 +0300

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -6,12 +6,15 @@ sudo -s
 # systemctl restart sshd
 lsblk
 sleep 5  # Waits 5 seconds.
-echo "You need more than maybe 50GB ıf space to build your own RPM packages and build custom bundles."
+echo "You need more than maybe 50GB ıf space to build your own RPM packages and$
 echo "You need to install storage utils to extend partition size?"
-select yn in "Yes (1)" "No (2)"; do
+options=("Yes" "No")
+select yn in "${options[@]}"
+do
     case $yn in
-        Yes ) sudo swupd bundle-add storage-utils; break;;
-        No ) exit;;
+        "Yes" ) sudo swupd bundle-add storage-utils; break;;
+        "No" ) exit;;
+            *) echo "Use 1 or 2 options";continue;;
     esac
 done
 echo "We will resize the partition now using parted and resizepart. First use ( p ) to print out the partitions."
